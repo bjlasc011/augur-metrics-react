741e360c4eb5869afb9d188d0bf2f392fd53a574
bjlasc011 <bjlasc01@gmail.com>
2018-08-11 21:57:30 -0400

    componentDidMount now calcs total shares outstanding

--- src/AugurMetrics.js
+++ src/AugurMetrics.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Info from "./components/info";
+import Info from "./info";
+import Chart from './chart';
+// import { getData } from "./utils"
+// import { TypeChooser } from "react-stockcharts/lib/helper";
 import './App.css';
 
 const Augur = require('augur.js');
 const augur = new Augur();
-// const BigNumber = require('bignumber.js');
 
 class AugurMetrics extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      ethId: undefined,
+      ethUsd: undefined,
       connectedAugurNode: undefined,
       etherUniverse: undefined,
       universe: undefined,
@@ -18,66 +22,112 @@ class AugurMetrics extends React.Component {
       marketIds: [],
       priceHistory: [],
       allSharesOutstanding: undefined,
+      chartData: undefined
     }
+    console.log("Constructor()")
   }
   componentDidMount() {
+    // http.get('https://api.coinmarketcap.com/v2/listings/', (err, res, body) => {
+    //   let bodyId = body;
+    //   console.log(bodyId);
+    //   console.log(bodyId[0].data);
+      // http(`https://api.coinmarketcap.com/v2/ticker/${this.state.ethId}/`, (err, response, body) => {
+      //   console.log(response);
+      //   console.log(body);
+      //   // this.setState({
+      //   //   ethUsd: body.data[1].quotes.USD.price
+      //   // })
+      //   // console.log(err);
+      // })
+      // if(err){
+      //   console.log(err);
+      // }).then(()=>{
+      //   console.log("then")
+      // })
     this.connectNode.then(() => {
-      this.getMarketCats()
+      // getData().then((data) => {
+        this.setState({
+          chartData: 0
+        })
+      // })
     })
     console.log("mounted");
   }
 
   connectNode = new Promise((resolve, reject) => {
-    var augurNode = //"wss://augur.gethstar.com";
+    var augurNode =     // "wss://augur.gethstar.com";
       "ws://127.0.0.1:9001";
     var ethereumNode = {
-      httpAddresses: [
-        // "http://127.0.0.1:8545",
+      httpAddresses: [  // "http://127.0.0.1:8545",
         "https://megageth.com/"
       ],
-      wsAddresses: [   // "ws://127.0.0.1:8546",
+      wsAddresses: [    // "ws://127.0.0.1:8546",
         "wss://megageth.com/ws"
       ]
     };
+
     augur.connect({ ethereumNode, augurNode }, (err, connectionInfo) => {
       console.log(connectionInfo);
       this.setState({
         connectedAugurNode: connectionInfo.augurNode,
         universe: connectionInfo.ethereumNode.contracts.Universe
       });
+
       if (connectionInfo) {
-        resolve(this.state.universe);
+        augur.markets.getCategories({ universe: connectionInfo.ethereumNode.contracts.Universe },
+          (error, cats) => {
+            let categories = [];
+            // let marks = [];
+            let shares = 0;
+            /************************************/
+            // Get Categories
+            /************************************/
+            for (let i = 0; cats[i] !== undefined; ++i) {
+              categories = [...categories, cats[i]]
+              this.setState({
+                categories: [...this.state.categories, cats[i]]
+              })
+            }
+            /************************************/
+            // Get Markets
+            /************************************/
+            for (let i = 0; categories[i] !== undefined; ++i) {
+              augur.markets.getMarketsInCategory({
+                universe: connectionInfo.ethereumNode.contracts.Universe,
+                category: categories[i].category
+              },
+                (err, markets) => {
+                  for (let j = 0; markets[j] !== undefined; ++j) {
+                    this.setState({
+                      marketIds: [...this.state.marketIds, markets[j]]
+                    })
+                  }
+                  /************************************/
+                  // Get SharesOutstanding
+                  /************************************/
+                  augur.markets.getMarketsInfo({ marketIds: [...markets] },
+                    (error, result) => {
+                      var num1, num2;
+                      for (let k = 0; result[k] !== undefined; ++k) {
+                        num1 = parseFloat(result[k].outstandingShares);
+                        num2 = parseFloat(shares);
+                        num1 += num2;
+                        shares = num1;
+                        this.setState({
+                          allSharesOutstanding: num1
+                        })
+                      }
+                    }
+                  )
+                }
+              )
+            }
+          }
+        )
       }
       else reject(err);
     })
   })
-
-  getSyncInfo = async () => {
-    await augur.augurNode.getSyncData(
-      (error, result) => {
-        console.log(result);
-        return result;
-      }
-    );
-  }
-
-  getMarketCats = async () => {
-    console.log(`fetching categories...${this.state.universe}`);
-    this.setState({
-      categories: []
-    })
-    augur.markets.getCategories({
-      universe: this.state.universe
-    },
-      (error, cats) => {
-        for (let i = 0; cats[i] !== undefined; ++i) {
-          this.setState({
-            categories: [...this.state.categories, cats[i]]
-          })
-        }
-        console.log(this.state.categories);
-      })
-  }
 
   getAllMarkets = async () => {
     console.log("fetching marketIds...");
@@ -150,11 +200,7 @@ class AugurMetrics extends React.Component {
     return (
       <div>
         <h1>AugurMetrics</h1>
-        <button onClick={this.getAllMarkets}>Get All Markets</button>
-        <br />
-        <button onClick={this.getMarketsInfo}>Get Shares Outstanding</button>
-        <br />
-
+        {this.state.connectedAugurNode === undefined && <p> Loading... </p>}
         <Info
           marketsIdsByCategories={this.state.marketsIdsByCategories}
           connectedAugurNode={this.state.connectedAugurNode}
@@ -163,6 +209,7 @@ class AugurMetrics extends React.Component {
           markets={this.state.markets}
           allSharesOutstanding={this.state.allSharesOutstanding}
         />
+        {/* <Chart /> */}
       </div>
     );
   }
